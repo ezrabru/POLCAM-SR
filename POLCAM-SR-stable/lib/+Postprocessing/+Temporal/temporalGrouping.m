89096d12f6c45e15a8c4b84de38c9c63766f719c
ezra bruggeman <ezra.bruggeman@gmail.com>
2023-03-17 04:29:27 +0000

    Remove basic copy

--- POLCAM-SR-stable/lib/+Postprocessing/+Temporal/temporalGrouping.m
+++
@@ -1,8 +0,0 @@
-function data = temporalGrouping(data,maxDist)
-%TEMPORALGROUPING
-
-[data,seed,tempGroupId,uniqueId] = Postprocessing.Temporal.temporalLinking(data,maxDist);
-
-data = Postprocessing.Temporal.groupLocalisations(data,seed,tempGroupId,uniqueId);
-
-end
